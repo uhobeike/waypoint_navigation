967dd9ab6586bff4d280527f20fee44448217c60
uhobeike <beike315@icloud.com>
2023-04-06 01:13:06 +0900

    Add access modifier (pub)

--- src/waypoint.rs
+++ src/waypoint.rs
@@ -1,27 +1,28 @@
+#[derive(Debug)]
 pub struct Waypoint {
-    x: f64,
-    y: f64,
-    yaw: f64,
+    pub x: f64,
+    pub y: f64,
+    pub yaw: f64,
 }
 
 impl Waypoint {
-    fn getX(&self) -> f64 {
+    pub fn get_x(&self) -> f64 {
         self.x
     }
 
-    fn getY(&self) -> f64 {
+    pub fn get_y(&self) -> f64 {
         self.y
     }
 
-    fn getYaw(&self) -> f64 {
+    pub fn get_yaw(&self) -> f64 {
         self.yaw
     }
 
-    fn getQuaternionW(&self) -> f64 {
+    pub fn get_quaternion_w(&self) -> f64 {
         (self.yaw / 2.0).cos()
     }
 
-    fn getQuaternionZ(&self) -> f64 {
+    pub fn get_quaternion_z(&self) -> f64 {
         (self.yaw / 2.0).sin()
     }
 }
