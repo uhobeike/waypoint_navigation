edd824748f953384239e7a0f58c84bb04b7a682b
uhobeike <beike315@icloud.com>
2023-04-06 23:07:50 +0900

    Rename file

--- src/send_goal.rs
+++ src/send_goal.rs
@@ -1,8 +1,9 @@
+use waypoint_navigation::Waypoint;
+
 use r2r::nav2_msgs::action::NavigateToPose;
 
 use std::sync::{Arc, Mutex};
-
-async fn client(arc_node: Arc<Mutex<r2r::Node>>, ) -> Result<(), r2r::Error> {
+pub async fn client(arc_node: Arc<Mutex<r2r::Node>>) -> Result<(), r2r::Error> {
     let (client, mut timer, service_available) = {
         let mut node = arc_node.lock().unwrap();
         let client = node.create_client::<NavigateToPose::SendGoal::Service>("/add_two_ints")?;
@@ -15,10 +16,19 @@ async fn client(arc_node: Arc<Mutex<r2r::Node>>, ) -> Result<(), r2r::Error> {
     println!("service available.");
     let uuid = r2r::unique_identifier_msgs::msg::UUID::default();
     let goal_pose = NavigateToPose::Goal::default();
-    let req = NavigateToPose::SendGoal::Request{goal_id: uuid, goal: goal_pose};
+    let req = NavigateToPose::SendGoal::Request {
+        goal_id: uuid,
+        goal: goal_pose,
+    };
     if let Ok(_resp) = client.request(&req).unwrap().await {
         println!("Debug");
     }
+
+    let waypoint = Waypoint {
+        x: 1.0,
+        y: 1.0,
+        yaw: 0.1,
+    };
     timer.tick().await?;
     Ok(())
 }
