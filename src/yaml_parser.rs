8749bc8904c6de05a49d8dd6081276b720ca00e4
uhobeike <beike315@icloud.com>
2023-04-10 12:21:24 +0900

    Feature function get_waypoint_debug

--- src/yaml_parser.rs
+++ src/yaml_parser.rs
@@ -1,11 +1,11 @@
 use waypoint_navigation::Waypoint;
 extern crate yaml_rust;
 use std::fs;
-use yaml_rust::{YamlEmitter, YamlLoader};
+use std::{collections::HashMap, hash::Hash};
+use yaml_rust::{yaml, YamlEmitter, YamlLoader};
 
 pub struct YamlParser {
     pub yaml_path: String,
-    // pub waypoint: [Waypoint],
     pub yaml: Vec<yaml_rust::Yaml>,
 }
 
@@ -16,4 +16,26 @@ impl YamlParser {
         let yaml = YamlLoader::load_from_str(&s).unwrap();
         self.yaml = yaml;
     }
+
+    pub fn get_waypoint(&self) -> Vec<Waypoint> {
+        let yaml = self.yaml[0].clone();
+
+        get_waypoint_debug(yaml["waypoints"].clone());
+
+        for waypoint in yaml["waypoints"].clone() {}
+
+        let waypoint = Vec::new();
+        waypoint
+    }
+}
+
+fn get_waypoint_debug(yaml: yaml_rust::Yaml) {
+    println!("{}", "Read Yaml Start");
+    for waypoint in yaml {
+        println!("{}", waypoint["id"].as_i64().unwrap());
+        println!("{}", waypoint["position"]["x"].as_f64().unwrap());
+        println!("{}", waypoint["position"]["y"].as_f64().unwrap());
+        println!("{}", waypoint["euler_angle"]["z"].as_f64().unwrap());
+    }
+    println!("{}", "Read Yaml End");
 }
