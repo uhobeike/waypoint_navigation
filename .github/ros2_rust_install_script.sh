0280124b73f5af29ff2f300595ab1c2e8bad3b35
uhobeike <beike315@icloud.com>
2023-04-17 17:24:32 +0900

    Feature github actions ci

---
+++ .github/ros2_rust_install_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xve
+
+sudo apt install curl gnupg2 lsb-release python3-pip git
+
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+sudo apt update -y
+sudo apt install -y ros-humble-desktop python3-colcon-common-extensions
+
+pip install git+https://github.com/tier4/colcon-cargo.git
+pip install git+https://github.com/colcon/colcon-ros-cargo.git
+
+. /opt/ros/humble/setup.bash
+
+git clone https://github.com/tier4/cargo-ament-build.git
+cd cargo-ament-build
+cargo install --path .
+cd ..
+rm cargo-ament-build -r
