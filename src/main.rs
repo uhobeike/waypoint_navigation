06d5a792f2fbc4c431fa635b6c3ece34f2ce1544
uhobeike <beike315@icloud.com>
2023-04-17 15:58:03 +0900

    Add yaml parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 pub mod send_goal;
 pub mod waypoint_server;
-use waypoint_navigation::Waypoint;
+pub mod yaml_parser;
 
-use colored::*;
 use std::sync::{Arc, Mutex};
 use tokio::task;
 
@@ -11,21 +10,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let action_server_name = "navigate_to_pose";
     let waypoint_navigation_flag = Arc::new(Mutex::new(false));
 
-    let goal = Waypoint {
-        x: 1.0,
-        y: -0.5,
-        yaw: 0.1,
+    let mut yaml = yaml_parser::YamlParser {
+        yaml_path: "/home/ikebe/ros2/rust/waypoint_navigation_ws/src/waypoint_navigation/config/waypoint.yaml".to_string(),
+        yaml: Vec::new(),
     };
 
-    println!(
-        "{} [x:{}, y:{}, yaw:{}, w:{}, z:{}]",
-        "Set Goal Point".green(),
-        goal.get_x(),
-        goal.get_y(),
-        goal.get_yaw(),
-        goal.get_quaternion_w(),
-        goal.get_quaternion_z()
-    );
+    yaml.load_yaml();
+    let waypoint = yaml.get_waypoint();
 
     let ctx = r2r::Context::create()?;
     let node = r2r::Node::create(ctx, "waypoint_navigation", "")?;
@@ -42,7 +33,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let an = arc_node.clone();
     let way_nav_flag = waypoint_navigation_flag.clone();
     task::spawn(async move {
-        send_goal::action_client(an, way_nav_flag, goal, &action_server_name.to_string())
+        send_goal::action_client(an, way_nav_flag, waypoint, &action_server_name.to_string())
             .await
             .unwrap()
     });
